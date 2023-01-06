933e928b1f7a7377b9b0489fc83807467575b9b1
dhlee <luvya8055@naver.com>
2023-01-06 12:34:55 +0900

    완성

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(function () {
+  $('.top_banner .xi-close').on('click', function () {
+    $('.top_banner').slideUp();
+  });
+
+  $(window).on('scroll', function () {
+    //스크롤량을 구함.
+    var sct = $(window).scrollTop();
+    // console.log(sct);
+    if (sct > 200) {
+      $('.header').addClass('fixed');
+    } else {
+      $('.header').removeClass('fixed');
+    }
+  });
+
   $('.product_slide').slick({
     slidesToShow: 4,
     variableWidth: true,
